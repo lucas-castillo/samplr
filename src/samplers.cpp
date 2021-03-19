0c2224a4f2a2f457c600c5fd15071312ada0d98f
Lucas Castillo <lucas.castillo@warwick.ac.uk>
2021-03-19 19:25:34 +0000

    export mh

--- src/samplers.cpp
+++ src/samplers.cpp
@@ -634,7 +634,7 @@ NumericMatrix build_tree(const NumericVector &theta, const NumericVector &moment
     return tree;
   }
 }
-
+//'@export
 //[[Rcpp::export]]
 List sampler_nuts_cpp(
     NumericVector start,
