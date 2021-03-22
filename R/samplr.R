69236dd283dfdc5efda29a78088b467fe0fc9fc6
Lucas Castillo <lucas.castillo@warwick.ac.uk>
2021-03-22 11:02:10 +0000

    add samplr.R

---
+++ R/samplr.R
@@ -0,0 +1,14 @@
+#' mypackage: A package for computating the notorious bar statistic.
+#'
+#' The mypackage package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section Mypackage functions:
+#' The mypackage functions ...
+#'
+#' @docType package
+#' @name samplr
+#' @importFrom Rcpp evalCpp
+#' @useDynLib samplr, .registration=TRUE
+NULL
+#> NULL
