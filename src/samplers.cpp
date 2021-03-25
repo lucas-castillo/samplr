800461f5fed3bc222f3ed4d7fb04620281255646
Lucas Castillo <lucas.castillo@warwick.ac.uk>
2021-03-25 11:35:57 +0000

    temp bespoke HMC

--- src/samplers.cpp
+++ src/samplers.cpp
@@ -454,6 +454,7 @@ List sampler_mc3_cpp(
 
   return List::create(chain, beta, swaps, NumericVector::create(swap_accepts, swap_attempts), acceptances / iterations);
 }
+
 ///'@export
 // [[Rcpp::export]]
 List sampler_hmc_cpp(
@@ -799,3 +800,66 @@ NumericVector gridDensity_cpp(
 
 //// TEST ////
 
+//'@export
+// [[Rcpp::export]]
+List sampler_hmc_cpp(
+    NumericVector start,
+    StringVector distr_name,
+    List distr_params,
+    double epsilon,
+    int L,
+    int iterations,
+    bool isMix,
+    NumericVector weights,
+    Function custom_func,
+    bool useCustom
+)
+{
+  // init vars
+  dfunc log_pdf = managePDF(distr_name, distr_params, isMix, weights, true, custom_func, useCustom);
+  int dim = start.size();
+  NumericMatrix chain(iterations, dim);
+  NumericMatrix momentums(iterations, dim);
+  int acceptances = 0;
+  chain.row(0) = start;
+  arma::mat identityMatrix(dim, dim, arma::fill::eye);
+  arma::colvec zeroes(dim, arma::fill::zeros);
+  arma::mat momentum_ = rmvnorm(1,  zeroes, identityMatrix);
+  NumericVector momentum = NumericVector(momentum_.begin(), momentum_.end());
+
+  for (int i = 1; i < iterations; i++){
+    // draw a sample of momentum
+
+    momentum_ = rmvnorm(1,  zeroes, identityMatrix);
+    momentum = NumericVector(momentum_.begin(), momentum_.end());
+    // initialize vars
+
+    NumericVector theta_prime  = chain.row(i-1);
+    NumericVector momentum_prime = momentum;
+
+
+    // leapfrog for each L step
+    leapfrog_step_cpp(theta_prime, momentum_prime, epsilon, log_pdf, L);
+
+    //
+    // Metropolis - Hastings Acceptance, using the joint density of position + momentum
+    //
+    double top =  exp(joint_d(theta_prime, momentum_prime, log_pdf));
+    double bottom =  exp(joint_d(chain.row(i-1), momentum_prime, log_pdf));
+
+    double alpha = top/bottom;
+    //
+
+    if (R::runif(0,1) <= alpha){
+      chain.row(i) = theta_prime;
+      momentums.row(i) = momentum_prime;
+      acceptances++;
+    } else{
+      chain.row(i) = chain.row(i-1);
+      momentums.row(i) = momentums.row(i-1);
+    }
+  }
+
+  return List::create(chain, momentums, ((double)(acceptances)/(double)(iterations)));
+
+}
