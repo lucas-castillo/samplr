b89b7ca9815b0c201f88cb9755bbd7e1c2846412
Lucas Castillo <lucas.castillo.marti@gmail.com>
2023-11-20 20:06:17 +0000

    test mchmc R call

--- tests/testthat/test-samplers.R
+++ tests/testthat/test-samplers.R
@@ -165,3 +165,18 @@ test_that("rec", {
     sampler_rec(0, "binom", c(0,1))
   )
 })
+
+test_that("mchmc", {
+  expect_no_error(
+    sampler_mchmc(0, "norm", c(0,1))
+  )
+  expect_no_error(
+    sampler_mchmc(0, custom_density = \(x){dnorm(x)})
+  )
+  expect_error(
+    sampler_mchmc(0, "norm", c(0,1), nChains = 2.5)
+  )
+  expect_error(
+    sampler_mchmc(0, "binom", c(0,1))
+  )
+})
