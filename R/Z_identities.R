50392f98171da869b28c24a694160b2316a72f05
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-01-17 15:06:05 +0000

    fix reference

--- R/Z_identities.R
+++ R/Z_identities.R
@@ -1,6 +1,6 @@
 #' Z Identities
 #' 
-#' Calculates identities Z1 to Z18 as defined in \insertCite{costello2016PeopleConditionalProbability, zhu2020BayesianSamplerGeneric:bibtex}{samplr}. If some of the probability estimates are not given, it will return NA for the identities than need them. 
+#' Calculates identities Z1 to Z18 as defined in \insertCite{costello2016PeopleConditionalProbability, zhu2020BayesianSamplerGeneric}{samplr}. If some of the probability estimates are not given, it will return NA for the identities than need them. 
 #' 
 #'
 #' @param a,b,a_and_b,a_or_b,a_given_b,b_given_a,a_given_not_b,b_given_not_a,a_and_not_b,b_and_not_a Probability estimates given by participants
