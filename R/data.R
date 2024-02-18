5864790beda3c892ccb4c9e67f8593717a18e10a
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-02-18 15:04:10 +0000

    document sundh2023e3

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Data from Experiment 3 in \insertCite{sundh2023UnifiedExplanationVariability}{samplr}
+#'
+#' \describe{
+#'   \item{ID}{}
+#'   \item{block}{3 blocks in total}
+#'   \item{trial}{Trial Number within a block}
+#'   \item{query, querydetail}{Verbal descriptions of the query}
+#'   \item{querytype}{Type of query: e.g. notBgA = p(¬B|A)}
+#'   \item{Estimate}{Estimated probability, in percentages}
+#'   \item{starttime, endtime}{}
+#'   \item{RT}{}
+#' }
+#' 
+"sundh2023e3"
