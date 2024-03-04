3b62734fd3503348bd0b65b1673e59403698fae8
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-03-04 16:10:11 +0000

    add licence info to sundh2023e3 data

--- R/data.R
+++ R/data.R
@@ -1,5 +1,6 @@
 #' Data from Experiment 3 in \insertCite{sundh2023UnifiedExplanationVariability}{samplr}
 #'
+#' This data are licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/), reproduced from materials in [OSF](https://osf.io/9kea6/).
 #' \describe{
 #'   \item{ID}{}
 #'   \item{block}{3 blocks in total}
