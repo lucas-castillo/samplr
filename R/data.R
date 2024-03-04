3ba5c405bfee193de3eb650e2af9c0c9176f3fd6
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-03-04 16:10:47 +0000

    fix references and re-document

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
-#' Data from Experiment 3 in \insertCite{sundh2023UnifiedExplanationVariability}{samplr}
-#'
+#' Data from Experiment 3 in \insertCite{sundh2023UnifiedExplanationVariability;textual}{samplr}
+#' 
 #' Participants made probability judgments of the format: “What is the probability that the weather is \[X\] on a random day in England?". Various weather events were used, and the queries included both marginal events, conditional events, conjunctions, and disjunctions. The total set of 20 unique queries formed a block within which the presentation order was randomized for each participant. The experiment consisted of three blocks, so that all participants responded to each unique query three times.
 #' 
 #' This data are licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/), reproduced from materials in [OSF](https://osf.io/9kea6/).
@@ -13,5 +13,7 @@
 #'   \item{starttime, endtime}{}
 #'   \item{RT}{}
 #' }
+#' @references 
+#' \insertRef{sundh2023UnifiedExplanationVariability}{samplr}
 #' 
 "sundh2023e3"
