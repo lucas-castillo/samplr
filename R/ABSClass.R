6ef5f2e919ba2ec10f9b1a32107efef03e4ee02d
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-04-15 09:52:46 +0100

    change references to \insertAllCited{}

--- R/ABSClass.R
+++ R/ABSClass.R
@@ -5,7 +5,7 @@
 #' @importFrom Rdpack reprompt
 #' 
 #' @references
-#'    \insertRef{zhuAutocorrelatedBayesian2023}{samplr}
+#'    \insertAllCited{}
 #'
 #' 
 CoreABS <- R6::R6Class("CoreABS",
@@ -58,8 +58,8 @@ CoreABS <- R6::R6Class("CoreABS",
 #' This Auto-correlated Bayesian Sampler model \insertCite{@ABS, @zhuAutocorrelatedBayesian2023}{samplr} is developed by Zhu.
 #' 
 #'
-#'@references
-#'    \insertRef{zhuAutocorrelatedBayesian2023}{samplr}
+#' @references
+#'    \insertAllCited{}
 #'
 #' @export
 #'
