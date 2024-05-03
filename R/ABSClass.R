3ea00b480e315b4a2c7ab9bdddd86b21bfec8f53
Yun-Xiao Li <yunxiao.li@outlook.com>
2024-05-03 16:16:57 +0100

    Add more explanation to the arguments.

--- R/ABSClass.R
+++ R/ABSClass.R
@@ -29,7 +29,7 @@ CoreABS <- R6::R6Class("CoreABS",
    #' @param n_chains an integer of the number of chains for the sampler.
    #' @param nd_time a numeric value of the non-decision time (in seconds).
    #' @param s_nd_time a numeric value of the inter-trial-variability of the non-decition time (in seconds).
-   #' @param distr_name a character string indicating the type of the posterior hypothesis distribution.
+   #' @param distr_name a character string indicating the type of the posterior hypothesis distribution. The package currently only support `norm`, which represents normal distribution.
    #' 
    #' @return A new 'CoreABS' object.
    #'
@@ -100,26 +100,26 @@ Zhu23ABS <- R6::R6Class(
     #' @description
     #' Simulate the ABS model.
     #' 
+    #' 
     #' @param stopping_rule a character string indicating the stopping rule of ABS to be applied. Possible values are `"fixed"` and `"relative"`. See also `Details`.
+    #' @param start_point a numeric vector setting the start point of each trial for the sampler. By default, it's set to `NA`, indicating that the starting point of the first trial is a random point from the posterior of hypotheses, and the starting points of subsequent trials are set to the last sample of the previous trial. For more detailed information, please refer to the vignette.
     #' @param ... further arguments passed to the ABS model, see also `Details`.
     #' 
     #' @details
     #' The ABS model has two types of stopping rules: fixed and relative. The fixed stopping rule means that a fixed number of samples are drawn to complete the tasks such as estimations and confidence intervals. On the other hand, the relative stopping rule means that the model counts the difference in evidence between the two hypotheses, and terminates the sampling process whenever the accumulated difference exceeds a threshold. This rule applies to tasks such as two-alternative force choice tasks.
     #' 
-    #' When `stopping rule` is `"fixed"`, the following arguments are required:
+    #' When the `stopping rule` is `"fixed"`, the following arguments are required:
     #' 
     #' - `n_sample` an integer of the fixed number of samples for each trial.
     #' - `trial_stim` a numeric vector of the stimulus of each trial.
-    #' - `start_point` a numeric vector setting the start point of each trial for the sampler. Defaults to `NA`, meaning that the start point of each trial is the last sample of the previous trial. Please refer to the vignette for more information.
     #' 
-    #' When the stopping rule is "relative", the following arguments are required:
+    #' When the `stopping rule` is `"relative"`, the following arguments are required:
     #' 
     #' - `delta` an integer of the relative difference between the number of samples supporting each hypothesis.
     #' - `dec_bdry` a numeric value of the decision boundary that separates the posterior hypothesis distribution.
     #' - `discrim` a numeric value of the stimuli discriminability.
-    #' - `trial_stim` a factor only consisting of two levels: below and above the decision boundary. It indicates the stimulus of each trial.
+    #' - `trial_stim` a factor that indicates the stimuli of each trial. It only consists of either one level or two levels. By definition, level 1 represents the stimulus below the decision boundary, while level 2 represents the stimulus above the decision boundary.
     #' - `prior_on_resp` a numeric vector for the Beta prior on responses. Defaults to `c(1,1)` representing the distribution `Beta(1,1)`.
-    #' - `start_point` a numeric vector setting the start point of each trial for the sampler. Defaults to `NA`, meaning that the start point of each trial is the last sample of the previous trial. Please refer to the vignette for more information.
     #' - `prior_depend` a boolean variable that control whether the prior on responses changes regarding the last stimulus. Defaults to `TRUE`. Please refer to the vignette for more information.
     #' - `max_iterations` an integer of the maximum length of the MC3 sampler. Defaults to 1000. The program will stop the sampling process after the length of the sampling sequence reaches to this limitation.
     #' 
@@ -154,16 +154,16 @@ Zhu23ABS <- R6::R6Class(
     #' zhuabs$simulate(stopping_rule='relative', delta = 4, dec_bdry = 0, discrim = 1, trial_stim = trial_stim)
     #' zhuabs$sim_results
     #' 
-    simulate = function(stopping_rule, ...){
+    simulate = function(stopping_rule, start_point=NA, ...){
       switch(
         stopping_rule,
         
         fixed = {
-          private$simulate_fixed_sr(...)
+          private$simulate_fixed_sr(start_point = start_point, ...)
         },
         
         relative = {
-          private$simulate_relative_sr(...)
+          private$simulate_relative_sr(start_point = start_point, ...)
         }
       ) # end of switch
       
@@ -192,6 +192,7 @@ Zhu23ABS <- R6::R6Class(
         stop("Please run the `estimate` method first.\n")
       } else {
         stopifnot("conf_level should be a single value between 0 and 1."=(is.numeric(conf_level) & length(conf_level) == 1 & conf_level >= 0 & conf_level <= 1))
+        # add a check of stopping rule
 
         conf_interval <- t(sapply(self$sim_results$samples, function(samples) quantile(samples, probs = c((1-conf_level)/2, (1+conf_level)/2))))
         self$sim_results$conf_interval_l <- conf_interval[,1]
@@ -211,7 +212,7 @@ Zhu23ABS <- R6::R6Class(
   
   private = list(
     
-    simulate_fixed_sr = function(n_sample, trial_stim, start_point=NA){
+    simulate_fixed_sr = function(n_sample, trial_stim, start_point){
       
       #Check inputs
       stopifnot("trial_stim should be a numeric vector."=(is.numeric(trial_stim)))
@@ -246,7 +247,7 @@ Zhu23ABS <- R6::R6Class(
       invisible(self)
     },
     
-    simulate_relative_sr = function(delta, dec_bdry, discrim, trial_stim, prior_on_resp = c(1,1), start_point=NA, prior_depend=TRUE, max_iterations=1000){
+    simulate_relative_sr = function(delta, dec_bdry, discrim, trial_stim, start_point, prior_on_resp = c(1,1), prior_depend=TRUE, max_iterations=1000){
       
       #Check inputs
       stopifnot("delta should be an integer."=(delta %% 1==0))
@@ -255,7 +256,7 @@ Zhu23ABS <- R6::R6Class(
       stopifnot("dec_bdry should be a single numeric value."=(is.numeric(dec_bdry) && length(dec_bdry) == 1))
       stopifnot("discrim should be a single numeric value."=(is.numeric(discrim) && length(discrim) == 1))
       stopifnot("trial_stim should be a factor."=is.factor(trial_stim))
-      stopifnot("trial_depend should be a boolean variable."=(isTRUE(prior_depend) || isFALSE(prior_depend)))
+      stopifnot("prior_depend should be a boolean variable."=(isTRUE(prior_depend) || isFALSE(prior_depend)))
       stopifnot("max_iterations should be a single numeric value."=(is.numeric(max_iterations) && length(max_iterations) == 1))
       
       trial_stim_num <- as.numeric(trial_stim)
