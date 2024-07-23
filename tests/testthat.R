1b825fff446ca1dbe22d49e52a639271d64712da
Lucas Castillo <lucas.castillo.marti@gmail.com>
2024-07-23 23:17:08 +0200

    Revert "add tests trick to avoid ubuntu issues"
    This reverts commit 444825866d9c5977d3959cd412784a17b23edb63.

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,7 +5,7 @@
 # Learn more about the roles of various files in:
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
-Sys.setenv("R_TESTS" = "")
+
 library(testthat)
 library(samplr)
 
